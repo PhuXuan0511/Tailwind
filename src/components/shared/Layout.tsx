0c87cdc73ed36e921eb18fb0e0ccfb526305f9c0
Phu Nguyen <162474701+PhuXuan0511@users.noreply.github.com>
2025-03-27 22:25:38 +0700

    Update Layout.tsx

--- src/components/shared/Layout.tsx
+++ src/components/shared/Layout.tsx
@@ -1,1 +1,15 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
 
+export function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <div>
+      <nav className="p-4 flex text-white items-center justify-between bg-gray-800">
+        <p className="text-3xl">
+          Tools<span className="text-purple-500"> FussSpring</span>
+        </p>
+      </nav>
+      <main>{children || <Outlet />}</main>
+    </div>
+  );
+}
